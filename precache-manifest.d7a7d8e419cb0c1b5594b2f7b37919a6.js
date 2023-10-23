b714043f901b7193b7d85fc78e2b939e558abf44
Vigneshraj77 <chitraselva33@gmail.com>
2023-10-24 00:45:00 +0530

    Updates

---
+++ precache-manifest.d7a7d8e419cb0c1b5594b2f7b37919a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda3710e5597cca1af59b7048f23cda9",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "f25a9dae9b8830abba7b",
+    "url": "/My-Portfolio/static/css/main.26663e78.chunk.css"
+  },
+  {
+    "revision": "d3de97ee2fb6405ce5de",
+    "url": "/My-Portfolio/static/js/2.dc354ff9.chunk.js"
+  },
+  {
+    "revision": "b1021e6c5ae3d4d99149d33588ddd0c4",
+    "url": "/My-Portfolio/static/js/2.dc354ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25a9dae9b8830abba7b",
+    "url": "/My-Portfolio/static/js/main.a8a04289.chunk.js"
+  },
+  {
+    "revision": "989754604da64f4fe7c8",
+    "url": "/My-Portfolio/static/js/runtime-main.49b6556d.js"
+  },
+  {
+    "revision": "8fceb3abcf91afdd5ec12994d7f4165c",
+    "url": "/My-Portfolio/static/media/icomoon.8fceb3ab.eot"
+  },
+  {
+    "revision": "9faddc170cc624bf52424ca89ab232c9",
+    "url": "/My-Portfolio/static/media/icomoon.9faddc17.svg"
+  },
+  {
+    "revision": "c74d019a3f19b1559d77c5120c7d9903",
+    "url": "/My-Portfolio/static/media/icomoon.c74d019a.ttf"
+  },
+  {
+    "revision": "f0ee518602a18f139c8464d257c18063",
+    "url": "/My-Portfolio/static/media/icomoon.f0ee5186.woff"
+  },
+  {
+    "revision": "1f7484b5a09f042799a2ab0ef028be10",
+    "url": "/My-Portfolio/static/media/leet.1f7484b5.webp"
+  },
+  {
+    "revision": "69f28077578e082c471e41eef6f97d25",
+    "url": "/My-Portfolio/static/media/pattern.69f28077.svg"
+  },
+  {
+    "revision": "e632617c4d9f2475d0774e341502b1a9",
+    "url": "/My-Portfolio/static/media/pp.e632617c.png"
+  }
+]);
